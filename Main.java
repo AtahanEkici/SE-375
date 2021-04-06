ed015b0aab117c100cdd60d9dd4b16a855da03c0
Ata <laythese@gmail.com>
2021-04-06 16:41:21 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,22 +9,36 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main 
 {
-    public static void main(String[] args) throws IOException
+    public static void LAB1()
     {
-        /* // LAB-1 //
-        Lab1.Setup("CLI"); 
-        */
-        
-        
+        try 
+        { 
+            Lab1.Setup("CLI");
+        } 
+        catch (IOException ex) 
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static void LAB2()
+    {
         ArrayList<File> files = FileOperation(splitter(catchInput())); // Catch files //
         Scanner scan_word = new Scanner(System.in);
         System.out.print("Plase input word : ");
         String scanned = scan_word.nextLine(); // catch keyword //
         Lab2_Runnable lab2 = new Lab2_Runnable(files,scanned);
         lab2.Start();
-        
+    }
+    
+    public static void main(String[] args) throws IOException
+    {
+       // LAB1();  
+       LAB2();
     }
 }
